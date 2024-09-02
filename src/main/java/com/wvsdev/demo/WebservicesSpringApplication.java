cc977d637401382b12c4f39e727d7b284c1c3718
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-02 16:14:08 -0300

    Initial commit

---
+++ src/main/java/com/wvsdev/demo/WebservicesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wvsdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvsdev/demo/WebservicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvsdev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
