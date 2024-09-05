37f439a5107c51a53bb83d79ffc777194572b7de
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-05 16:51:39 -0300

    Exception handling - findById

--- src/main/java/com/wvsdev/demo/services/UserService.java
+++ src/main/java/com/wvsdev/demo/services/UserService.java
@@ -2,6 +2,7 @@ package com.wvsdev.demo.services;
 
 import com.wvsdev.demo.domain.entities.User;
 import com.wvsdev.demo.repositories.UserRepository;
+import com.wvsdev.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
