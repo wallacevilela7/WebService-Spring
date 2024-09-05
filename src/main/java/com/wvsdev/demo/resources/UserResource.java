6dae9b894b2bcf3ec5f2b66376d77eda2078748b
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-05 16:26:47 -0300

    User update

--- src/main/java/com/wvsdev/demo/resources/UserResource.java
+++ src/main/java/com/wvsdev/demo/resources/UserResource.java
@@ -37,8 +37,15 @@ public class UserResource {
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
